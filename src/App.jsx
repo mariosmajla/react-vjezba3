0ef6bc8f57f53efa721de23be28dc957b35b3de6
Mario Smajla <mariosmajla@gmail.com>
2023-03-07 21:27:51 +0100

    Dodano ternary i and

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,27 @@ import DrugaKompa from './components/DrugaKompa';
 import {TrecaKompa, CetvrtaKompa} from './components/OstaleKompe';
 
 function App() {
+  //const jasnoSvima = false; - primjer za ternary operator
+  const jasnoSvima = 23;
+  
+  //ternary operator
+  const podnaslov = jasnoSvima 
+  ? 
+  <h2>Svima je jasno, baš super</h2> 
+  : 
+  <h2>Svi samo klimaju glavom, nikome nije jasno</h2>;
+
+  //and operator
+  const pozdravniFooter = jasnoSvima && <h2>Pozdrav do četvrtka</h2>;
   return (
     <div className="App">
       <h1>Algebra</h1>
+      {podnaslov}
       <PrvaKompa />
       <DrugaKompa />
       <TrecaKompa />
       <CetvrtaKompa />
+      {pozdravniFooter}
     </div>
   );
 }
