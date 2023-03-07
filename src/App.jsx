0801ef73d18891edfa32813b9936b352d54d530d
Mario Smajla <mariosmajla@gmail.com>
2023-03-07 20:04:50 +0100

    dodane promjene

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import PrvaKompa from './components/PrvaKompa';
 import DrugaKompa from './components/DrugaKompa';
+import {TrecaKompa, CetvrtaKompa} from './components/OstaleKompe';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <h1>Algebra</h1>
       <PrvaKompa />
       <DrugaKompa />
+      <TrecaKompa />
+      <CetvrtaKompa />
     </div>
   );
 }
---
+++ src/components/OstaleKompe/index.jsx
@@ -0,0 +1,19 @@
+export const TrecaKompa = () => {
+    return (
+        <div>
+            <h1>Treća komponenta</h1>
+        </div>
+    )
+    }
+    
+    export const CetvrtaKompa = () => {
+        return (
+            <div>
+                <h1>Četvrta komponenta</h1>
+            </div>
+        )
+        }
+        
+        
+
+    //export default TrecaKompa;
